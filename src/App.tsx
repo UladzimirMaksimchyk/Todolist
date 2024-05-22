05ea8cd37454a375591d99470a7c92efdc18cff1
Uladzimir <boba70325@gmail.com>
2024-05-22 21:33:45 +0300

    Update "function APP"

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,18 @@ function App() {
         { id: 3, title: "Three", IsDone: false },
         { id: 4, title: "Four", IsDone: false },
     ];
-    const tasks1: Array<TaskType> = [
-        { id: 1, title: 'HTML&CSS', IsDone: true },
-        { id: 2, title: 'JS', IsDone: true },
-        { id: 3, title: 'ReactJS', IsDone: false },
-        { id: 4, title: 'Redux', IsDone: false },
-    ]
+    // const tasks1: Array<TaskType> = [
+    //     { id: 1, title: 'HTML&CSS', IsDone: true },
+    //     { id: 2, title: 'JS', IsDone: true },
+    //     { id: 3, title: 'ReactJS', IsDone: false },
+    //     { id: 4, title: 'Redux', IsDone: false },
+    // ]
 
 
     return (
         <div className="App">
             <Todolist title={"One"} tasks={tasks} />
-            <Todolist title={"Two"} tasks={tasks1} />
+            {/* <Todolist title={"Two"} tasks={tasks1} /> */}
 
         </div>
     );
