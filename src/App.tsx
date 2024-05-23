14514bb6fabfbfaad522d1af2c24a4e48fb462c3
Uladzimir <boba70325@gmail.com>
2024-05-23 22:13:26 +0300

    Created "tasks2"

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { TaskType, Todolist } from './todolist/Todolist';
 
 
 
+
+
 function App() {
 
     const tasks: Array<TaskType> = [
@@ -13,18 +15,24 @@ function App() {
         { id: 3, title: "Three", IsDone: false },
         { id: 4, title: "Four", IsDone: false },
     ];
-    // const tasks1: Array<TaskType> = [
-    //     { id: 1, title: 'HTML&CSS', IsDone: true },
-    //     { id: 2, title: 'JS', IsDone: true },
-    //     { id: 3, title: 'ReactJS', IsDone: false },
-    //     { id: 4, title: 'Redux', IsDone: false },
-    // ]
+    const tasks1: Array<TaskType> = [
+        { id: 1, title: 'HTML&CSS', IsDone: true },
+        { id: 2, title: 'JS', IsDone: true },
+        { id: 3, title: 'ReactJS', IsDone: false },
+        { id: 4, title: 'Redux', IsDone: false },
+    ]
+    const tasks2: Array<TaskType> = [
+        { id: 1, title: 'HTML&CSS', IsDone: true },
+        { id: 2, title: 'JS', IsDone: true },
+        { id: 3, title: 'ReactJS', IsDone: false },
+        { id: 4, title: 'Redux', IsDone: false },
+    ]
 
 
     return (
         <div className="App">
             <Todolist title={"One"} tasks={tasks} />
-            {/* <Todolist title={"Two"} tasks={tasks1} /> */}
+            <Todolist title={"Two"} tasks={tasks1} />
 
         </div>
     );
